11f8f856ae0fd052c9bf48fb3f1df029c0b1673b
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-08-16 17:05:10 -0500

    databse get and put

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,15 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const textDb = await openDB('text', 1);
+  const tx = textDb.transaction('text', 'readwrite');
+  const store = tx.objectStore('text');
+  const request = store.put({ id: id, text: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
 export const getOneDb = async (id) => {
   console.log('GET from the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readonly');
-  const store = tx.objectStore('todos');
+  const textDb = await openDB('text', 1);
+  const tx = textDb.transaction('text', 'readonly');
+  const store = tx.objectStore('text');
   const request = store.get(id);
   const result = await request;
   console.log('result.value', result);
